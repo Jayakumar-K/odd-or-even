f06da7b3ab4eab188b94fa302bc68c6d1d9ade42
Jayakumar-K <jayakumar9595@gmail.com>
2016-06-07 11:17:51 +0530

    Create OE.c
    easy method

---
+++ OE.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%2==1)
+    {
+        printf("its odd");
+    }
+    else
+    {
+        printf("its even");
+    }
+    return 0;
+}
