f8156579af1443cfb25dbe5fd979555c7fa46ce2
Jayakumar-K <jayakumar9595@gmail.com>
2016-06-07 11:22:45 +0530

    Update and rename README.md to ODD.C

---
+++ ODD.C
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%2==1)
+    {
+        printf("its odd");
+    }
+    else
+    {
+        printf("its even");
+    }
+    return 0;
+}
+
